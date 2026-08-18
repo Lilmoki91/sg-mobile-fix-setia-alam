76efcfb407299c3460a7053f202eb2a021c520f4
Johannes Hadamard (MACGYVER CODERMAN) <khairuldinsuyitno@gmail.com>
2026-08-19 01:49:26 +0800

    Update Facebook share link in script.js

--- script.js
+++ script.js
@@ -1222,7 +1222,7 @@ function initFacebookOverlay() {
     
     setTimeout(() => {
       closeOverlay();
-      window.open('https://www.facebook.com', '_blank');
+      window.open('https://www.facebook.com/sharer/sharer.php?u=${url}', '_blank');
     }, 1000);
     
     setTimeout(() => {
