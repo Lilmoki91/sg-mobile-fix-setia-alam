f553cd6e8bd2bc8b42045496c0bdb4568f3ec4c4
Johannes Hadamard (MACGYVER CODERMAN) <khairuldinsuyitno@gmail.com>
2026-09-04 01:29:23 +0800

    Fix service worker registration path

--- frontend/script.js
+++ frontend/script.js
@@ -1276,7 +1276,7 @@ function initFacebookOverlay() {
 // Daftar Service Worker
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js')
+      navigator.serviceWorker.register('service-worker.js')
         .then(registration => {
           console.log('Service Worker registered with scope:', registration.scope);
         })
