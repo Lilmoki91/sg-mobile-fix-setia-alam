c31d37b373c79da2a6231d3db7281336e8f01357
Johannes Hadamard (MACGYVER CODERMAN) <khairuldinsuyitno@gmail.com>
2026-09-03 23:09:34 +0800

    Update service-worker.js

--- frontend/service-worker.js
+++ frontend/service-worker.js
@@ -1,5 +1,5 @@
 // Nama cache (versi) - Tukar jika ada kemaskini
-const CACHE_NAME = 'sg-mobile-fix-v1';
+const CACHE_NAME = 'sg-mobile-fix-v1.5';
 
 // Senarai fail yang hendak di-cache
 const urlsToCache = [
