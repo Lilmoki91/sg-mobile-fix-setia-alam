03b3d490f791559bc44166c8ffc6f06f08a0c6f0
Johannes Hadamard (MACGYVER CODERMAN) <khairuldinsuyitno@gmail.com>
2026-09-05 02:49:53 +0800

    Update service worker registration log to V8

--- frontend/script.js
+++ frontend/script.js
@@ -1277,7 +1277,7 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('sw.js')
       .then(reg => {
-        console.log('✅ SW V7 registered');
+        console.log('✅ SW V8 registered');
 
         // Paksa update
         reg.update();
